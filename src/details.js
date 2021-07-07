adf3587261f8555d5d677837054b937c9109f83c
Meghana3570 <meghanagogi2001@gmail.com>
2021-07-07 19:35:37 +0530

    first commit

---
+++ src/details.js
@@ -0,0 +1,24 @@
+var details = [
+{
+    id:"1",
+    name: "Malikarjuna Appartments",
+    address: "h.no:88,Hyder Nagar,Telangana",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRATdoDPy7f-QO6TbLX62TRYLCcHKHW982hTA&usqp=CAU"
+},
+{
+    id:"2",
+    name: "Venkat Appartments",
+    address: "h.no:88,Hyderabad,Telangana",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRATdoDPy7f-QO6TbLX62TRYLCcHKHW982hTA&usqp=CAU"
+},
+{
+    id:"3",
+    name: "Sai Nilayam",
+    address: "h.no:88,Bora Banda,Hyderabad,Telangana",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRATdoDPy7f-QO6TbLX62TRYLCcHKHW982hTA&usqp=CAU"
+},
+]
+
+
+
+export default details;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import details from './details.js'
+
+
+// Search Result
+function Display(props){
+    return (
+        <div>
+            <div class="card">
+            <div class="row no-gutters">
+                <div class="col-6 imageCol">
+                    <img src={props.image}></img>
+                </div>
+                <div class="col-6">
+                <div class="card-body">
+                    <h4>{props.name}</h4>
+                    <p>{props.address}</p>
+                    <a href="rental.html" class="btn btn1" role="button">Contact Owner</a>
+                    <a href="#" class="btn btn1 btn2" role="button">Add to Whislist</a>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+// Navbar
+function NavBar(){
+    return (
+        <div>
+            <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+                <a class="navbar-brand" href="index.html">Home</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Post my advertisement </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Sign Up</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+function Footer1(){
+    return(
+        <div>
+            <footer>
+                <p>Copyright ⓒ 2021</p>
+            </footer>
+        </div>
+    );
+}
+
+function createDetail(detail){
+    return <Display key={detail.id} name={detail.name} address={detail.address} image={detail.image}></Display>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    details.map(createDetail)
+    ,document.getElementById("cardDetails")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <NavBar></NavBar>
+    ,document.getElementById("navBar")
+);
+
+ReactDOM.render(
+    <Footer1></Footer1>
+    ,document.getElementById("footer")
+);
+
+
+
+
+
+
